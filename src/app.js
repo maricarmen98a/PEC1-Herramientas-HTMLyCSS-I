ff98bf7776e5752018682ca82c427af45be6c1f4
carmen <maricarmen98a@gmail.com>
2022-04-04 18:00:04 +0200

    hecho

--- src/app.js
+++ src/app.js
@@ -16,6 +16,6 @@ function preloadImages(){
 
 }
 
-preloadImages("personajes.jpg", "money-heist.jpg", "berlin.png", "denver.png", "dinero.png", "dni.png", "heroe.png", "hospital.png", "money-heist2.jpg", "profesor.png", "raquel.png", "rio.png", "tokio.png");
+preloadImages("personajes.jpg", "money-heist.jpg", "berlin.png", "denver.png", "dinero.png", "dni.png", "heroe.png", "hospital.png", "money-heist2.jpg", "profesor.png", "raquel.png", "rio.png", "tokio.png", "amigos.png", "amor.jpg", "anillos.png", "busqueda.png", "cena.png", "coche.png", "esposas.png", "huella.jpg", "ladron.png", "lisboa.png", "llamada.png", "madre.png", "muerto.png", "mujer-policia.png", "pastillas.jpg", "pastillas2.png", "phone.jpg", "prision.png", "programas.jpg", "puerta.png", "puño.png", "robo.png", "tokio.png");
 
 
