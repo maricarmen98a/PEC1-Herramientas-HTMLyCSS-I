a4e4474f383d973076fdb282ec94573af1df8aae
carmen <maricarmen98a@gmail.com>
2022-04-04 17:50:39 +0200

    hecho

--- src/app.js
+++ src/app.js
@@ -17,3 +17,5 @@ function preloadImages(){
 }
 
 preloadImages("personajes.jpg", "money-heist.jpg", "berlin.png", "denver.png", "dinero.png", "dni.png", "heroe.png", "hospital.png", "money-heist2.jpg", "profesor.png", "raquel.png", "rio.png", "tokio.png");
+
+
